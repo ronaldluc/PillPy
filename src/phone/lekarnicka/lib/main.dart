cbeec20572ba9a5baa4c08235478a6cd374350ed
Petr Zelina <zelina.pe@gmail.com>
2020-11-21 08:15:25 +0100

    add phone project src :(

---
+++ src/phone/lekarnicka/lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+import 'scanner.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      title: 'Lékárnička',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: DrugScanner(
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   CameraController _controller;
+//   Future<void> _initializeControllerFuture;
+//
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     // To display the current output from the Camera,
+//     // create a CameraController.
+//     _controller = CameraController(
+//       // Get a specific camera from the list of available cameras.
+//       widget.camera,
+//       // Define the resolution to use.
+//       ResolutionPreset.high,
+//       enableAudio: false,
+//     );
+//
+//     // Next, initialize the controller. This returns a Future.
+//     _initializeControllerFuture = _controller.initialize();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: FutureBuilder<void>(
+//         future: _initializeControllerFuture,
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             // If the Future is complete, display the preview.
+//             return CameraPreview(_controller);
+//           } else {
+//             // Otherwise, display a loading indicator.
+//             return Center(child: CircularProgressIndicator());
+//           }
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
---
+++ src/phone/lekarnicka/lib/scanner.dart
@@ -0,0 +1,186 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+// A screen that allows users to take a picture using a given camera.
+class DrugScanner extends StatefulWidget {
+  final CameraDescription camera;
+
+  const DrugScanner({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  DrugScannerState createState() => DrugScannerState();
+}
+
+class DrugScannerState extends State<DrugScanner> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  static double buttonHeight = 80;
+  static double fontSize = 30;
+  bool isAccepting = true;
+  Timer photoTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.high,
+      enableAudio: false,
+    );
+
+    print("Initing camera");
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+
+    Timer(Duration(seconds: 1), () => _runRecognition(null));
+    // starting timer
+    photoTimer = Timer.periodic(
+      Duration(seconds: 10),
+      _runRecognition,
+    );
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _runRecognition(Timer t) async {
+    print("------- Running recognition");
+    var photo = await _takePhoto();
+    if (photo == null) {
+      return;
+    }
+
+    var status = await _sendImage(photo);
+    if (status == null) {
+      return;
+    } else if (status == "nothing") {
+      File(photo).delete();
+      print("File $photo deleted.");
+      return;
+    }
+
+    print("Setting status $status");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Lékárnička')),
+      body: Stack(
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the Future is complete, display the preview.
+                return CameraPreview(_controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.all(10),
+            child: ButtonTheme(
+              height: buttonHeight,
+              minWidth: double.infinity,
+              buttonColor: Colors.green,
+              child: RaisedButton(
+                child: Text(
+                  "Seznam léků",
+                  style: TextStyle(fontSize: fontSize),
+                ),
+                onPressed: () => null,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.all(10),
+            child: ButtonTheme(
+              height: buttonHeight,
+              minWidth: double.infinity,
+              buttonColor: Colors.green,
+              child: RaisedButton(
+                child: Text(
+                  "Přidat lék manuálně",
+                  style: TextStyle(fontSize: fontSize),
+                ),
+                onPressed: () => null,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<String> _takePhoto() async {
+    try {
+      await _initializeControllerFuture;
+
+      final path = join(
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.png',
+      );
+
+      await _controller.takePicture(path);
+      return path;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<String> _sendImage(path) async {
+    print("Sending image $path");
+
+    var url = Uri.parse("http://baiku.cz:4444");
+    var request = new http.MultipartRequest("POST", url);
+    request.fields['user'] = 'someone@somewhere.com';
+    request.files.add(await http.MultipartFile.fromPath(
+      'package',
+      path,
+      contentType: MediaType('application', 'x-tar'),
+    ));
+    var req = request.send();
+    print("Request sent");
+    try {
+      var response = await req
+          .timeout(const Duration(seconds: 5));
+      print("Got response $response");
+      if (response.statusCode == 200) {
+        return response.stream.bytesToString();
+      }
+      return "nothing";
+    } on TimeoutException catch (_) {
+      print("Request timeout");
+      return "nothing";
+    } on SocketException catch (_) {
+      print("Socket error");
+      return "nothing";
+    }
+  }
+}
